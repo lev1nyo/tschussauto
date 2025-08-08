b264828544e0d7a8d931456b6c645ce573cb87bd
Andrii Levitan <andriylevitan@gmail.com>
2025-08-08 19:05:37 +0200

    fix: обновил конфиг sitemap с правильным доменом

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://tschüssauto.de',
+  siteUrl: 'https://xn--tschssauto-deb.de', // 🔁 Punycode обязательно
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapBaseFileName: 'sitemap',
   exclude: ['/api/*'],
-}
+};
