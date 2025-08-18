5ff5a4d734ea954590bac4267905a83d3cf6d394
Andrii Levitan <andriylevitan@gmail.com>
2025-08-18 20:09:05 +0200

    Fix: single MyApp export and GA4 init in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,15 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+// pages/_app.js
 import '../styles/globals.css'
 import Script from 'next/script'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-// можно хранить в .env.local как NEXT_PUBLIC_GA_ID, но оставлю твой ID по умолчанию
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-X57G2DK9T6'
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
-  // фиксируем просмотры при клиентских переходах Next.js
+  // счётчик просмотров при клиентских переходах
   useEffect(() => {
     const handleRouteChange = (url) => {
       if (typeof window.gtag === 'function') {
@@ -29,12 +22,12 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      {/* GA4 загрузка скрипта */}
+      {/* GA4 загрузка */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
       />
-      {/* инициализация GA4 */}
+      {/* Инициализация GA4 */}
       <Script id="ga4-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
